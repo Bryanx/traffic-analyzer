217ffc08b8848866c12fdbbf32874f245c8ff614
Bryan <br.deridder@gmail.com>
2018-09-26 00:10:51 +0200

    Added test

--- simulator/src/test/java/be/kdg/simulator/SimulatorApplicationTests.java
+++ simulator/src/test/java/be/kdg/simulator/SimulatorApplicationTests.java
@@ -1,5 +1,6 @@
 package be.kdg.simulator;
 
+import be.kdg.simulator.config.GeneratorConfig;
 import be.kdg.simulator.messaging.generators.MessageGenerator;
 import be.kdg.simulator.model.CameraMessage;
 import org.junit.Test;
@@ -16,10 +17,19 @@ public class SimulatorApplicationTests {
 	@Autowired
 	private MessageGenerator messageGenerator;
 
+	@Autowired
+	private GeneratorConfig generatorConfig;
+
 	@Test
-	public void testMessageGenerator() {
+	public void testLicensePlate() {
 		CameraMessage cameraMessage = messageGenerator.generate();
 		Assert.assertTrue(cameraMessage.getLicensePlate().matches("^[1-8]-[A-Z]{3}-[0-9]{2}[1-9]$"));
 	}
 
+	@Test
+	public void testMaxId() {
+		CameraMessage cameraMessage = messageGenerator.generate();
+		int maxId = generatorConfig.getMaxid();
+		Assert.assertTrue(cameraMessage.getCameraId() <= maxId);
+	}
 }
