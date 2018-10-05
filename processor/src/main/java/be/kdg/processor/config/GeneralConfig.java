e7f5f05152285ec2dab3f3a6263f408752b5f3d2
Bryan <br.deridder@gmail.com>
2018-10-05 10:18:15 +0200

    Added modelmapper

--- processor/src/main/java/be/kdg/processor/config/GeneralConfig.java
+++ processor/src/main/java/be/kdg/processor/config/GeneralConfig.java
@@ -2,6 +2,7 @@ package be.kdg.processor.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -18,5 +19,10 @@ public class GeneralConfig {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
--- processor/src/main/java/be/kdg/processor/config/mappers/CameraMessageMapper.java
+++ processor/src/main/java/be/kdg/processor/config/mappers/CameraMessageMapper.java
@@ -1,4 +1,17 @@
 package be.kdg.processor.config.mappers;
 
+import be.kdg.processor.config.dtos.CameraMessageDTO;
+import be.kdg.processor.domain.CameraMessage;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
 public class CameraMessageMapper {
+    private final ModelMapper modelMapper;
+
+    public CameraMessage convertToEntity(CameraMessageDTO msgDto) {
+        return modelMapper.map(msgDto, CameraMessage.class);
+    }
 }
