ab32b90de99a28a76609260142ff9b5d70fc96c6
Bryan <br.deridder@gmail.com>
2018-10-05 08:11:21 +0200

    Added test for licenseplate service

--- processor/src/test/java/be/kdg/processor/services/ThirdParty/ProxyLicensePlateServiceImplTest.java
+++ processor/src/test/java/be/kdg/processor/services/ThirdParty/ProxyLicensePlateServiceImplTest.java
@@ -6,20 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ProxyCameraServiceImplTest {
+public class ProxyLicensePlateServiceImplTest {
 
     //Add '--add-modules java.xml.bind' to VM options before running.
     @Autowired
-    private ProxyCameraServiceImpl proxyCameraService;
+    private ProxyLicensePlateServiceImpl proxyLicensePlateService;
+
+    private static final String TEST_PLATE = "4-ABC-123";
 
     @Test
     public void get() {
-        String result = proxyCameraService.get(3);
+        String result = proxyLicensePlateService.get(TEST_PLATE);
         System.out.println(result);
-        assertTrue("Expected to get a result when calling CameraServiceProxy, but got: " + result, result != null);
+        assertTrue("Expected to get a result when calling LicensPlateProxyService, but got: " + result,
+                result != null);
     }
 }
