77c27fcd0618f32de551bdfd1fa0e1db28ac35fc
Bryan <br.deridder@gmail.com>
2018-10-07 14:59:03 +0200

    Added test for custom query

---
+++ processor/src/test/java/be/kdg/processor/camera/message/CameraMessageRepositoryTest.java
@@ -0,0 +1,32 @@
+package be.kdg.processor.camera.message;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CameraMessageRepositoryTest {
+
+    @Autowired
+    private CameraMessageRepository cameraMessageRepository;
+
+    @Test
+    public void findAllByTimestampBetween() {
+        CameraMessage msg1 = new CameraMessage();
+        msg1.setCameraId(5);
+        msg1.setLicensePlate("2-ABC-123");
+        msg1.setTimestamp(LocalDateTime.now());
+        cameraMessageRepository.save(msg1);
+        List<CameraMessage> allByTimestampBetween = cameraMessageRepository.findAllByTimestampBetween(LocalDateTime.now().minusSeconds(200), LocalDateTime.now());
+        System.out.println(allByTimestampBetween);
+        assertEquals(allByTimestampBetween.size(), 1);
+    }
+}
