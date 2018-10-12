7af220ebfe9af1bf6f6c99a0be1a80c64888cee6
Bryan <br.deridder@gmail.com>
2018-10-12 15:17:41 +0200

    Added more controller endpoints

--- processor/src/main/java/be/kdg/processor/fine/FineController.java
+++ processor/src/main/java/be/kdg/processor/fine/FineController.java
@@ -6,6 +6,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
@@ -20,10 +25,30 @@ public class FineController {
         return new ResponseEntity<>(modelMapper.map(fine, FineDTO.class), HttpStatus.OK);
     }
 
+    @RequestMapping(value = "/fines", params = { "from", "to" }, method = GET)
+    public ResponseEntity<FineDTO[]> getFilteredFines(@RequestParam(value = "from") Optional<String> from,
+                                                      @RequestParam(value = "to") Optional<String> to) {
+        List<Fine> fines;
+//        if (!from.isPresent() && !to.isPresent()) fines = fineService.findAll();
+//        else if (!from.isPresent()) fines = fineService.findAllByCreationDateBetween(LocalDateTime.MIN, to.get());
+//        else if (!to.isPresent()) fines = fineService.findAllByCreationDateBetween(from.get(), LocalDateTime.MAX);
+//        else fines = fineService.findAllByCreationDateBetween(from.get(), to.get());
+//
+//        return new ResponseEntity<>(modelMapper.map(fines.toArray(), FineDTO[].class), HttpStatus.OK);
+        return null;
+    }
+
     @PostMapping("/fines")
     public ResponseEntity<FineDTO> createFine(@RequestBody FineDTO fineDTO) {
         Fine fineIn = modelMapper.map(fineDTO, Fine.class);
-        Fine fineOut = fineService.saveAndFlush(fineIn);
+        Fine fineOut = fineService.save(fineIn);
         return new ResponseEntity<>(modelMapper.map(fineOut, FineDTO.class), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/fines/{id}/approve")
+    public ResponseEntity<FineDTO> toggleApproveFine(@PathVariable int id) throws FineException {
+        Fine fineOut = fineService.findById(id);
+        fineOut.setApproved(!fineOut.isApproved());
+        return new ResponseEntity<>(modelMapper.map(fineOut, FineDTO.class), HttpStatus.OK);
     }
 }
