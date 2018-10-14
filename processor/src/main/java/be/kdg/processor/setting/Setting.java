bfe1dba379032a3c50f1fe2cd07216ee23576cb6
Bryan <br.deridder@gmail.com>
2018-10-14 11:59:43 +0200

    Removed id from setting

--- processor/src/main/java/be/kdg/processor/setting/Setting.java
+++ processor/src/main/java/be/kdg/processor/setting/Setting.java
@@ -3,17 +3,12 @@ package be.kdg.processor.setting;
 import lombok.Data;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
-import static javax.persistence.GenerationType.IDENTITY;
 
 @Data
 @Entity
 public class Setting {
     @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private int id;
     private String key;
     private int value;
 }
