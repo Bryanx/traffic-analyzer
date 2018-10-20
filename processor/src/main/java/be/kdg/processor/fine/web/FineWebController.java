e44be9651bbf0aa7d71b61cc5984b3a6b8abfe51
Bryan <br.deridder@gmail.com>
2018-10-20 19:19:53 +0200

    Added basic fine details page

--- processor/src/main/java/be/kdg/processor/fine/web/FineWebController.java
+++ processor/src/main/java/be/kdg/processor/fine/web/FineWebController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 @RequiredArgsConstructor
@@ -17,5 +18,11 @@ public class FineWebController {
     public ModelAndView getAllFines() {
         FineDTO[] fines = modelMapper.map(fineService.findAll().toArray(), FineDTO[].class);
         return new ModelAndView("fines", "fines", fines);
+    }
+
+    @GetMapping("/fines/details/{id}")
+    public ModelAndView GetFine(@PathVariable int id) throws FineException {
+        FineDTO fine = modelMapper.map(fineService.findById(id), FineDTO.class);
+        return new ModelAndView("fines/details", "fine", fine);
     }
 }
