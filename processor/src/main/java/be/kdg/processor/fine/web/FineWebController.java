afb1507ee28f660154720c3b2049abc51e06bf6c
Bryan <br.deridder@gmail.com>
2018-10-21 12:19:55 +0200

    Added form for changing fines

--- processor/src/main/java/be/kdg/processor/fine/web/FineWebController.java
+++ processor/src/main/java/be/kdg/processor/fine/web/FineWebController.java
@@ -1,12 +1,13 @@
 package be.kdg.processor.fine.web;
 
+import be.kdg.processor.fine.Fine;
 import be.kdg.processor.fine.FineService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RequiredArgsConstructor
 @Controller
@@ -24,5 +25,15 @@ public class FineWebController {
     public ModelAndView GetFine(@PathVariable int id) throws FineException {
         FineDTO fine = modelMapper.map(fineService.findById(id), FineDTO.class);
         return new ModelAndView("fines/details", "fine", fine);
+    }
+
+    @PostMapping("/fines/{id}")
+    public ModelAndView updateFine(@PathVariable int id, @ModelAttribute FineDTO fineDTO) throws FineException {
+        Fine fine = fineService.findById(id);
+        fine.setApproved(fineDTO.isApproved());
+        fine.setComment(fineDTO.getComment());
+        fine.setPrice(fineDTO.getPrice());
+        Fine fineOut = fineService.save(fine);
+        return new ModelAndView(new RedirectView("/fines"), "fineOut", fineOut);
     }
 }
