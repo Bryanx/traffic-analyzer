8cde1e6fa290e770bb97d9697e74cdf84a1d96a4
Bryan <br.deridder@gmail.com>
2018-11-06 21:07:20 +0100

    Added doc

--- processor/src/main/java/be/kdg/processor/shared/livestats/LiveStatsController.java
+++ processor/src/main/java/be/kdg/processor/shared/livestats/LiveStatsController.java
@@ -5,6 +5,9 @@ import be.kdg.processor.fine.FineService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+/**
+ * This controller will add a 'data' model to an existing webpage where live statistics can be shown.
+ */
 @RequiredArgsConstructor
 public class LiveStatsController {
     public final FineService fineService;
